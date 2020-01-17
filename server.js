5aad54e8f0cbdb3fe4ece34a2d9ff78f6080a42b
boranorben <zhotkp@gmail.com>
2020-01-17 18:27:05 +0900

    add home/durationQuery route

--- server.js
+++ server.js
@@ -7,10 +7,12 @@ const cors = require('cors')
 
 const PORT = 3001
 
+const homeRoute = require('./routes/home')
 const forumsRoute = require('./routes/forums')
 
 app.use(cors())
 app.use(bodyParser.json())
 app.listen(PORT, () => console.log('Server is running on Port: ' + PORT))
 
+app.use('/home', homeRoute)
 app.use('/forums', forumsRoute)
