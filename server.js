e7d7041064698eb07209d29992dd311ac0aaa2f2
boranorben <zhotkp@gmail.com>
2020-05-23 23:38:21 +0700

    update col

--- server.js
+++ server.js
@@ -1,5 +1,25 @@
 require('dotenv').config()
 
+var current_date = new Date()
+current_date.setDate(current_date.getDate() - 3)
+current_date = current_date.toISOString().slice(0, 10).replace(/-/g, '')
+
+String.prototype.replaceBetween = function (start, end, what) {
+  return this.substring(0, start) + what + this.substring(end);
+}
+
+const col = process.env.MONGODB_THREADS_COLLECTION.replaceBetween(18, 28, current_date)
+const db = require('monk')(process.env.MONGODB_URI, {
+  authSource: 'admin'
+})
+
+const threads_col = db.get(col)
+threads_col.find({}).then((doc) => {
+  if (doc.length != 0) {
+    process.env['MONGODB_THREADS_COLLECTION'] = col
+  }
+})
+
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
